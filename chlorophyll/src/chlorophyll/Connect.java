447063ac4374f528c62b9a44ba08e36f879eb1c4
Allie Kieras <Akieras16@holynames-sea.org>
2016-02-02 19:21:42 -0800

    first commit for 3 February 2016 milestone connects to azure database and runs single select query

---
+++ chlorophyll/src/chlorophyll/Connect.java
@@ -0,0 +1,147 @@
+package chlorophyll;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+/**
+ * This class demonstrates how to connect to MySQL and run some basic commands.
+ * 
+ * In order to use this, you have to download the Connector/J driver and add
+ * its .jar file to your build path.  You can find it here:
+ * 
+ * http://dev.mysql.com/downloads/connector/j/
+ * 
+ * You will see the following exception if it's not in your class path:
+ * 
+ * java.sql.SQLException: No suitable driver found for jdbc:mysql://localhost:3306/
+ * 
+ * To add it to your class path:
+ * 1. Right click on your project
+ * 2. Go to Build Path -> Add External Archives...
+ * 3. Select the file mysql-connector-java-5.1.24-bin.jar
+ *    NOTE: If you have a different version of the .jar file, the name may be
+ *    a little different.
+ *    
+ * The user name and password are both "root", which should be correct if you followed
+ * the advice in the MySQL tutorial. If you want to use different credentials, you can
+ * change them below. 
+ * 
+ * You will get the following exception if the credentials are wrong:
+ * 
+ * java.sql.SQLException: Access denied for user 'userName'@'localhost' (using password: YES)
+ * 
+ * You will instead get the following exception if MySQL isn't installed, isn't
+ * running, or if your serverName or portNumber are wrong:
+ * 
+ * java.net.ConnectException: Connection refused
+ */
+public class Connect {
+
+	/** The name of the MySQL account to use (or empty for anonymous) */
+	private final String userName = "b5fc8fcedc1161";
+
+	/** The password for the MySQL account (or empty for anonymous) */
+	private final String password = "1d92f039";
+
+	/** The name of the computer running MySQL */
+	private final String serverName = "us-cdbr-azure-west-c.cloudapp.net";
+
+	/** The port of the MySQL server (default is 3306) */
+	private final int portNumber = 3306;
+
+	/** The name of the database we are testing with (this default is installed with MySQL) */
+	private final String dbName = "chlorophyll";
+	
+	/** The name of the table we are testing with */
+	private final String tableName = "testcsv1replaced";
+	
+	/**
+	 * Get a new database connection
+	 * 
+	 * @return
+	 * @throws SQLException
+	 */
+	public Connection getConnection() throws SQLException {
+		Connection conn = null;
+		Properties connectionProps = new Properties();
+		connectionProps.put("user", this.userName);
+		connectionProps.put("password", this.password);
+
+		conn = DriverManager.getConnection("jdbc:mysql://"
+				+ this.serverName + ":" + this.portNumber + "/" + this.dbName,
+				connectionProps);
+		
+
+		return conn;
+	}
+
+	/**
+	 * Run a SQL command which does not return a recordset:
+	 * CREATE/INSERT/UPDATE/DELETE/DROP/etc.
+	 * 
+	 * @throws SQLException If something goes wrong
+	 */
+	public boolean executeUpdate(Connection conn, String command) throws SQLException {
+	    Statement stmt = null;
+	    try {
+	        stmt = conn.createStatement();
+	        stmt.executeUpdate(command); // This will throw a SQLException if it fails
+	        return true;
+	    } finally {
+
+	    	// This will run whether we throw an exception or not
+	        if (stmt != null) { stmt.close(); }
+	    }
+	}
+	
+	/**
+	 * Connect to MySQL and do some stuff.
+	 */
+	public void run() throws SQLException{
+
+		// Connect to MySQL
+		Connection conn = null;
+		try {
+			conn = this.getConnection();
+			System.out.println("Connected to database");
+		} catch (SQLException e) {
+			System.out.println("ERROR: Could not connect to the database");
+			e.printStackTrace();
+			return;
+		}
+
+		// Create a table
+		
+		Statement stmt = null;
+		
+		String query = "SELECT number " + 
+						"from " + dbName + ".testcsv1replaced";
+		try {
+		    stmt = conn.createStatement();
+		    ResultSet rs = stmt.executeQuery(query);
+		    while(rs.next()){
+		    	double number = rs.getDouble("number");
+		    	System.out.println("Looking at latitude " + number);
+		    }
+	    } catch (SQLException e) {
+			System.out.println("ERROR: Could not create the table");
+			e.printStackTrace();
+			return;
+		}finally{
+			if(stmt != null){ stmt.close();}
+		} 
+	
+	}
+	
+	/**
+	 * Connect to the DB and do some stuff
+	 */
+	public static void main(String[] args) throws SQLException {
+		Connect app = new Connect();
+		app.run();
+	}
+}
