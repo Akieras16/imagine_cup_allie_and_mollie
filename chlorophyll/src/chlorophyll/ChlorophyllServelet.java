9e47b88daab4d0e8bc74fec89e864f7efd7dda15
Allie Kieras <Akieras16@holynames-sea.org>
2016-04-25 07:17:52 -0700

    color map

--- chlorophyll/src/chlorophyll/ChlorophyllServelet.java
+++ chlorophyll/src/chlorophyll/ChlorophyllServelet.java
@@ -86,31 +86,117 @@ public class ChlorophyllServelet extends HttpServlet implements Servlet {
 					double db = nums[i][j];
 					
 					if(db != -1){
+						int red = 0;
+						int green = 0; 
+						int blue = 0;
 						
-					double holder = db * 100;
-			
+						if(db > .1 && db < .3){
+							red = 122;
+							green = 5;
+							blue = 247;
+						}
 						
-					
-					int calc = (int)holder;
-					if(calc > 0){
-					System.out.println(calc);
-					}
-					int green = 0 + calc;
-					int blue = 200 - calc;
-					int red = 0;
-					
-					if(green > 255){
-						green = 255;
-					}
-					
-					
-					if(blue < 0){
-						blue = 0;
-					}
-					
-					if(red < 0){
-						red = 0;
-					}
+						if(db > .3 && db < .5){
+							red = 89;
+							green = 9;
+							blue = 219;
+						}
+						
+						if(db > .5 && db < .7){
+							red = 54;
+							green = 5;
+							blue = 232;
+						}
+						
+						if(db > .7 && db < .9){
+							red = 27;
+							green = 62;
+							blue = 222;
+						}
+						
+						if( db > .9 && db < 1.1){
+							red = 7;
+							green = 183;
+							blue = 237;
+						}
+						
+						if(db > 1.1 && db < 1.3){
+							red = 7;
+							green = 237;
+							blue = 237;
+						}
+						
+						if(db > 1.3 && db < 1.5){
+							red = 7;
+							green = 237;
+							blue = 210;
+						}
+						
+						if(db > 1.5 && db < 1.7){
+							red = 7;
+							green = 237;
+							blue = 95;
+						}
+						
+						if(db > 1.7 && db < 1.9){
+							red = 7; 
+							green = 237;
+							blue = 26;
+						}
+						
+						if(db > 1.9 && db < 2.1){
+							red = 80;
+							green = 237;
+							blue = 7;
+						}
+						
+						if(db > 2.1 && db < 2.3){
+							red = 149;
+							green = 237;
+							blue = 7;
+						}
+						
+						if(db > 2.3 && db < 2.5){
+							red = 214;
+							green = 237;
+							blue = 7;
+						}
+						
+						if(db > 2.5 && db < 2.7){
+							red = 237;
+							green = 233;
+							blue = 7;
+						}
+						
+						if(db > 2.7 && db < 2.9){
+							red = 237;
+							green = 180;
+							blue = 7;
+						}
+						
+						if(db > 2.9 && db < 3.1){
+							red = 237;
+							green = 153;
+							blue = 7;
+						}
+						
+						if(db > 3.1 && db < 3.3){
+							red = 237;
+							green = 88;
+							blue = 7;
+						}
+						
+						if(db > 3.3 && db < 3.5){
+							red = 237;
+							green = 57;
+							blue = 7;
+						}
+						
+						if(db > 3.5){
+							red = 237;
+							green = 38;
+							blue = 7;
+						}
 					Color color = new Color(red, green, blue);
 					graphics.setColor(color);
 					graphics.drawLine(j, i, j, i);
